80104733039696a6f2b37837eab717e8be605278
Sfoucheur <s.foucheur@gmail.com>
2023-03-31 17:18:59 +0200

    Refactor : Added asyncForEach utils to the array extensions

--- src/utils/arrayExtensions.ts
+++ src/utils/arrayExtensions.ts
@@ -4,6 +4,7 @@ import { SkChoice } from '@src/types/toolbox/prompts-tools';
 declare global {
   interface Array<T> {
     toPromptChoices(this: T[], titleKey?: string | number | undefined): SkChoice<T>[];
+    asyncForEach(this: T[], callback: (element: T, index: number, array: T[]) => Promise<void>): Promise<void>;
   }
 }
 
@@ -21,4 +22,14 @@ Array.prototype.toPromptChoices = function <T>(this: T[], titleKey = undefined) 
     }
     return result;
   }, []);
+};
+
+/**
+ * async Array.Foreach
+ * @param callback Callback to call on each Element
+ */
+Array.prototype.asyncForEach = async function <T>(this: T[], callback: (element: T, index: number, array: T[]) => Promise<void>) {
+  for (let index = 0; index < this.length; index++) {
+    await callback(this[index], index, this);
+  }
 };
